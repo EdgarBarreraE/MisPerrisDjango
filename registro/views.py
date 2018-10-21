0317ceffc95589db7d49dc423898388057bddfde
Unknown <edg.barrera@alumnos.duoc.cl>
2018-10-21 16:47:36 -0300

    Mis Perris Django Listo para trabajar

--- registro/views.py
+++ registro/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html',{})
+    return render(request,'index.html',{'nombre':"Edgar"})
 
 def form(request):
     return render(request,'form.html',{})
+
+def crear(request):
+    nombre = request.POST.get('nombre','')
+    return HttpResponse("nombre' : " + nombre)
