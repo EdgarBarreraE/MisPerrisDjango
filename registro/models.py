5b82431e36c2e69011c5dca68226e26ae68d137d
Millaray <carmen.rojasc@alumnos.duoc.cl>
2018-10-30 21:54:25 -0300

    Prueba 2

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    rut = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    fechaNacimiento = models.DateField('%m/%d/%Y')
+    telefono = models.IntegerField()
+    tipoCasa = models.CharField(max_length=100)
+    region = models.CharField(max_length=80)
+    comuna = models.CharField(max_length=80)
