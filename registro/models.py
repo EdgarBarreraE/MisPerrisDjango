011016d406eafc9a71e89b809cd069bfa54e2ce9
Millaray <carmen.rojasc@alumnos.duoc.cl>
2018-11-02 14:21:35 -0300

    Create Models

--- registro/models.py
+++ registro/models.py
@@ -7,9 +7,17 @@ class Usuario(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     rut = models.CharField(max_length=100)
-    mail = models.CharField(max_length=100)
-    fechaNacimiento = models.DateField('%m/%d/%Y')
+    email = models.EmailField(max_length=100)
+    fechaNacimiento = models.DateField()
     telefono = models.IntegerField()
     tipoCasa = models.CharField(max_length=100)
-    region = models.CharField(max_length=80)
-    comuna = models.CharField(max_length=80)
+    region = models.CharField(max_length=100)
+    comuna = models.CharField(max_length=100)
+
+
+class Rescatado(models.Model):
+    foto = models.ImageField(upload_to='fotos/')
+    nombre = models.CharField(max_length=30)
+    raza = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    estado = models.IntegerField(default=1)
