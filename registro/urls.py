32d0a3f7e7a7b4ba113bf2e0798d39b71bdd26f0
Millaray <carmen.rojasc@alumnos.duoc.cl>
2018-11-03 01:25:16 -0300

    Registro

--- registro/urls.py
+++ registro/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('Index',views.index,name="index"),
     path('form/',views.form,name="form"),
     path('',views.Inicio,name="Inicio"),
-    path('form_rescatado/',views.form_rescatado,name="form_rescatado")
-
+    path('form_rescatado/', views.form_rescatado, name="form_rescatado"),
+    path('form_rescatado/crear_rescatado', views.crear_rescatado, name="crear_rescatado"),
+    path('form_rescatado/lista_rescatados', views.lista_rescatados, name="lista_rescatados"),
+    path('form_rescatado/editar/<int:id>',views.editar, name="editar"),
+    path('form_rescatado/editado/<int:id>',views.editado, name="editado")
 ]
--- registro/views.py
+++ registro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import Usuario
+from .models import Rescatado
 
 # Create your views here.
 
@@ -18,3 +20,42 @@ def Inicio(request):
 
 def form_rescatado(request):
     return render(request,'form_rescatado.html',{})
+
+def crear_rescatado(request):
+    foto = request.FILES['foto']
+    nombre = request.POST.get('nombre', '')
+    raza = request.POST.get('raza', '')
+    descripcion = request.POST.get('descripcion', '')
+    estado = request.POST.get('estado', 1)
+    perro = Rescatado(foto=foto,nombre=nombre,raza=raza,descripcion=descripcion,estado=estado)
+    perro.save()
+    return HttpResponse("El perrito "+nombre+" de raza "+raza+", ha sido correctamente registrado")
+
+def lista_rescatados(request):
+    return render(request,'lista_rescatados.html',{'elementos':Rescatado.objects.all()})
+
+def editar(request,id):
+    perro = Rescatado.objects.get(pk=id)
+    return render(request,'editar.html',{'perro':perro})
+
+def editado(request,id):
+    perro = Rescatado.objects.get(pk=id)
+    foto = request.FILES['foto']
+    nombre = request.POST.get('nombre', '')
+    raza = request.POST.get('raza', '')
+    descripcion = request.POST.get('descripcion', '')
+    estado = request.POST.get('estado', 1)
+    perro.nombre = nombre
+    perro.raza = raza
+    perro.descripcion = descripcion
+    perro.estado = estado
+    perro.foto = foto
+    perro.save()
+    return HttpResponse("El perrito "+nombre+" ha sido sido editado")
+
+"""
+def eliminar_perro(request,id)
+perro = Rescatado.objects.get(pk=id)
+perro.delete()
+return HttpResponse("Perrito ha sido eliminado de los registros.")
+"""
