74e6938bc3f850a546311cf6547e8d4379fd64cc
Edgar <edg.barrera@alumnos.duoc.cl>
2018-11-05 17:33:55 -0300

    AuthAvancePass

--- MisperrisCRUD/settings.py
+++ MisperrisCRUD/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ebarreraespinoza@gmail.com'
+EMAIL_HOST_PASSWORD = '*redrevolver12'
+DEFAULT_FROM_EMAIL = 'django.core.mail.backends.smtp.EmailBackend>'
